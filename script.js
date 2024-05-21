11ebf341ab263c8f37e585e573483f008fe1e3c7
crivera0712 <125428748+crivera0712@users.noreply.github.com>
2024-05-20 17:52:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// Create textarea
 var textarea = document.createElement('textarea');
 textarea.id = 'textInput';
 textarea.placeholder = 'Enter your text here';
 document.getElementById('root').appendChild(textarea);
 
-// Create submit button
 var submitBtn = document.createElement('button');
 submitBtn.id = 'submitBtn';
 submitBtn.textContent = 'Submit';
 document.getElementById('root').appendChild(submitBtn);
 
-// Event listener for submit button
-document.getElementById('submitBtn').addEventListener('click', function() {
+submitBtn.addEventListener('click', function() {
   var textInput = document.getElementById('textInput').value;
   var words = textInput.split(/\s+/);
   var wordFreq = {};
-  
+
   // Count word frequencies
   words.forEach(function(word) {
     if (word in wordFreq) {
@@ -30,8 +27,13 @@ document.getElementById('submitBtn').addEventListener('click', function() {
     return wordFreq[b] - wordFreq[a];
   });
 
+  var existingResult = document.getElementById('resultTable');
+  if (existingResult) {
+    existingResult.remove();
+  }
 
-  var resultHTML = '<table><tr><th>Word Name</th><th>Word Frequency</th></tr>';
+  // Create result table
+  var resultHTML = '<table id="resultTable"><tr><th>Word</th><th>Frequency</th></tr>';
   for (var i = 0; i < Math.min(5, sortedWords.length); i++) {
     var word = sortedWords[i];
     resultHTML += '<tr><td>' + word + '</td><td>' + wordFreq[word] + '</td></tr>';
@@ -39,6 +41,5 @@ document.getElementById('submitBtn').addEventListener('click', function() {
   resultHTML += '</table>';
   document.getElementById('root').innerHTML += resultHTML;
 
-  
   console.log(wordFreq);
 });
