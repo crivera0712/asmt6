54ec9a3c6ffd27f0a1c25016eed6aadb7b008177
Christian <christian.rivera0712@gmail.com>
2024-05-23 21:57:10 -0700

    fixed table not resetting after every button press

--- script.js
+++ script.js
@@ -8,38 +8,40 @@ submitBtn.id = 'submitBtn';
 submitBtn.textContent = 'Submit';
 document.getElementById('root').appendChild(submitBtn);
 
-submitBtn.addEventListener('click', function() {
-  var textInput = document.getElementById('textInput').value;
-  var words = textInput.split(/\s+/);
-  var wordFreq = {};
+document.getElementById('root').addEventListener('click', function(event) {
+  if (event.target && event.target.id === 'submitBtn') {
+    var textInput = document.getElementById('textInput').value;
+    var words = textInput.split(/\s+/);
+    var wordFreq = {};
 
-  // Count word frequencies
-  words.forEach(function(word) {
-    if (word in wordFreq) {
-      wordFreq[word]++;
-    } else {
-      wordFreq[word] = 1;
+    // Count word frequencies
+    words.forEach(function(word) {
+      if (word in wordFreq) {
+        wordFreq[word]++;
+      } else {
+        wordFreq[word] = 1;
+      }
+    });
+
+    // Sort word frequencies
+    var sortedWords = Object.keys(wordFreq).sort(function(a, b) {
+      return wordFreq[b] - wordFreq[a];
+    });
+
+    var existingResult = document.getElementById('resultTable');
+    if (existingResult) {
+      existingResult.remove();
     }
-  });
 
-  // Sort word frequencies
-  var sortedWords = Object.keys(wordFreq).sort(function(a, b) {
-    return wordFreq[b] - wordFreq[a];
-  });
+    // Create result table
+    var resultHTML = '<table id="resultTable"><tr><th>Word</th><th>Frequency</th></tr>';
+    for (var i = 0; i < Math.min(5, sortedWords.length); i++) {
+      var word = sortedWords[i];
+      resultHTML += '<tr><td>' + word + '</td><td>' + wordFreq[word] + '</td></tr>';
+    }
+    resultHTML += '</table>';
+    document.getElementById('root').innerHTML += resultHTML;
 
-  var existingResult = document.getElementById('resultTable');
-  if (existingResult) {
-    existingResult.remove();
+    console.log(wordFreq);
   }
-
-  // Create result table
-  var resultHTML = '<table id="resultTable"><tr><th>Word</th><th>Frequency</th></tr>';
-  for (var i = 0; i < Math.min(5, sortedWords.length); i++) {
-    var word = sortedWords[i];
-    resultHTML += '<tr><td>' + word + '</td><td>' + wordFreq[word] + '</td></tr>';
-  }
-  resultHTML += '</table>';
-  document.getElementById('root').innerHTML += resultHTML;
-
-  console.log(wordFreq);
 });
